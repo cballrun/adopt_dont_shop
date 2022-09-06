42701e7cd297112ff1678012b1bb7a90a19ca5d0
cballrun <106977961+cballrun@users.noreply.github.com>
2022-09-06 17:15:26 -0400

    Revert "Add default attribute test for application_pet model, add validation …"

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,6 +4,20 @@ class Application < ApplicationRecord
   attribute :app_status, default: "In Progress"
   validates_presence_of :name, :street_address, :city, :state, :zip_code
 
+  # aasm column: :status do
+  #   state :in_progress, initial: true, display: 'In Progress'
+  #   state :pending, display: 'Pending'
+  #   state :approved, display: 'Approved'
+
+  #   event :submit do
+  #     transitions from: :in_progress, to: :pending
+  #   end
+
+  #   event :approve do
+  #     transitions from: :pending, to: :approved
+  #   end
+  # end
+
   # TODO: make this a class (self) method and remove Pet.all so it can be chained
   def search_pet(name)
     Pet.all.where("name ILIKE ?", "%#{name}%")
