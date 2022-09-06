735a40d9e0faef6c7d2584b90d6ad81c54b22adb
Carter Ball <carter.k.ball@gmail.com>
2022-09-06 14:29:37 -0400

    Add default attribute test for application_pet model, add validation tests for application model

--- spec/models/application_pet_spec.rb
+++ spec/models/application_pet_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe ApplicationPet, type: :model do
     it {should belong_to :application}
     it {should belong_to :pet}
   end
+  
+  describe 'default attributes' do
+    it 'initializes with the correct default status' do
+      app_1 = Application.create!(name: "Carter Ball", street_address: "123 Easy Street", city: "Atlanta", state: "GA", zip_code: 30307, status: "In Progress", description: "something")
+      shelter_1 = Shelter.create!(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+      pet_1 = shelter_1.pets.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true)
+      pet_app_1 = ApplicationPet.create!(application: app_1, pet: pet_1)
+
+      expect(pet_app_1.status).to eq("Pending")
+    end
+  end
+
+  
 end
